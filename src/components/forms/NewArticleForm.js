4298e42498c96bdfb90c2c5449ca44ec0b69bed6
Samuel Muli <samuelmuli95@gmail.com>
2023-11-07 23:13:22 +0300

    Add NewArticle form

---
+++ src/components/forms/NewArticleForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NewArticleForm(){
+    return(
+        <div className="new-article-form">
+            <h2>Create New Article</h2>
+            {/* {Add form elements here} */}
+        </div>
+    )
+}
+
+export default NewArticleForm
