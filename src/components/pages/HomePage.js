bc53068bd2fc027d8886cbee24dc70a6fd3853c5
Samuel Muli <samuelmuli95@gmail.com>
2023-11-07 23:08:59 +0300

    Add home page

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ArticleList from "../../ArticleList";
+
+function HomePage(){
+    return(
+        <div className="home-page">
+            <ArticleList />
+        </div>
+    )
+}
+
+export default HomePage
