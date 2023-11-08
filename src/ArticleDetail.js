c4afaf67519db917e4a3a3090ace04893d3f19e6
Samuel Muli <samuelmuli95@gmail.com>
2023-11-08 13:06:18 +0300

    Add delete functionality

--- src/ArticleDetail.js
+++ src/ArticleDetail.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 function ArticleDetail(){
+    const navigate = useNavigate()
     const { id } = useParams()
     console.log('ID:', id)
     const [article, setArticle] = useState(null)
@@ -16,9 +17,21 @@ function ArticleDetail(){
         return <div>Loading...</div>
     }
 
+    function handleDelete(){
+        fetch(`http://localhost:3000/articles/${id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(() => {
+            console.log('Article deleted')
+            navigate(`/`)
+        })
+    }
+
     return(
         <div className="article-detail">
             <Link to={`/article/${article.id}/edit`}>Edit</Link>
+            <button onClick={handleDelete}>Delete</button>
             <h2>{article.title}</h2>
             <p>{article.content}</p>
         </div>
