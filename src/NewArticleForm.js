25eeae86271c04fa7cb4595f4b408f5df3aed423
Samuel Muli <samuelmuli95@gmail.com>
2023-11-08 11:09:35 +0300

    Add form submission handler function

--- src/NewArticleForm.js
+++ src/NewArticleForm.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewArticleForm(){
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        fetch('http://localhost:3000/articles', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({title, content})
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log('New article added:', data)
+            setTitle('')
+            setContent('')
+        })
+        .catch(error => console.log(error.message))
+    }
+
     return(
         <div className="new-article-form">
             <h2>Create New Article</h2>
