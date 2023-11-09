ff443886bbb503cc53b36c06e0bb4382b62c5cea
Samuel Muli <samuelmuli95@gmail.com>
2023-11-09 13:17:03 +0300

    Add Navlink styles

--- src/Header.js
+++ src/Header.js
@@ -7,9 +7,10 @@ function Header(){
             <h1>MULIVERSE</h1>
             <p>Get a sneak peek of the amazing world that is my mind!</p>
             <nav>
-                <ul>
-                    <NavLink to="/" exact>Home</NavLink>
-                    <NavLink to="/new">Add New Article</NavLink>
+                <ul className="nav">
+                    <NavLink to="/" exact className="nav-item" activeClassName="is-active">Home</NavLink>
+                    <NavLink to="/new" className="nav-item" activeClassName="is-active">Add New Article</NavLink>
+                    <span className="nav-indicator"></span>
                 </ul>
             </nav>
         </div>
